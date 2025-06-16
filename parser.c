97c7b661f6ee6e857d17f72590f0f7c3b5fb198a
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-16 18:46:51 +0300

    wip parser

---
+++ parser.c
@@ -0,0 +1,20 @@
+#include "so_long.h"
+
+t_map *parse_map(char *name)
+{
+	t_map *map;
+	char *str_map;
+
+	if (!name)
+		return NULL;
+	ft_printf("name: %s\n", name);
+	str_map = read_from_file(name);
+	ft_printf("map: \n%s\n", str_map);
+	if (!str_map || !validate_map(name, str_map))	
+	{
+		free(str_map);
+		err_msg("Invalid map", 0);
+		return NULL;
+	}
+
+}
