a87b18e6db57b62b532ae29835b56f0993f9489d
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-16 18:51:51 +0300

    wip

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
-#include <mlx.h>
+#include "so_long.h"
 
-int main()
+int  hello(int keycode, t_mlx_ctx *mlx)
 {
-	void	*mlx;
-	void	*mlx_win;
+	ft_printf("Hello world key: %d", keycode);
+	if (keycode == 27)
+		mlx_destroy_window(mlx->mlx, mlx->window);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 600, 600, "So long");
-	mlx_loop(mlx);
+	return 0;
+}
+
+int main(int argc, char **args)
+{
+	t_ctx *ctx;
+
+	if (argc < 1)
+		exit(1);	
+	ctx = malloc(sizeof(t_ctx));
+	if (!ctx)
+		exit_with_err(ENOMEM);
+	parse_map(args[1]);
+	// ctx->mlx->mlx = mlx_init();
+	// ctx->mlx->window = mlx_new_window(ctx->mlx->mlx, 600, 600, args[0]);
+	// mlx_hook(ctx->mlx->window, 2, 1L<<0, &hello, ctx);
+	// mlx_loop(ctx->mlx->mlx);
 }
