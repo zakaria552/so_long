7b24fb9e7b49e11478d2efa8f3b5577bfc838571
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-18 22:46:37 +0300

    added func to load assets

---
+++ textures.c
@@ -0,0 +1,67 @@
+#include "so_long.h"
+
+bool load_textures(t_ctx *ctx)
+{
+	t_tiles *tiles;
+
+	tiles = malloc(sizeof(t_tiles));
+	if (!tiles)
+	{
+		err_msg("", ENOMEM);
+		return false;
+	}
+	tiles->txt_empty = mlx_load_png("./textures/empty.png");
+	if (!tiles->txt_empty)
+	{
+		free(tiles);
+		err_msg("Failed to load texture", errno);
+		return false;		
+	}
+	tiles->empty = mlx_texture_to_image(ctx->mlx, tiles->txt_empty);
+	if (!tiles->empty)
+	{
+		free(tiles->empty);
+		free(tiles);
+		err_msg("Failed to turn texture to image", errno);
+		return false;		
+	}
+	tiles->txt_wall = mlx_load_png("./textures/wall.png");
+	if (!tiles->txt_wall)
+	{
+		err_msg("Failed to load texture", errno);
+		return false;		
+	}
+	tiles->wall = mlx_texture_to_image(ctx->mlx, tiles->txt_wall);
+	if (!tiles->wall)
+	{
+		err_msg("Failed to turn texture to image", errno);
+		return false;		
+	}
+	tiles->txt_coin = mlx_load_png("./textures/coin.png");
+	if (!tiles->txt_coin)
+	{
+		err_msg("Failed to load texture", errno);
+		return false;		
+	}
+	tiles->coin = mlx_texture_to_image(ctx->mlx, tiles->txt_coin);
+	if (!tiles->coin)
+	{
+		err_msg("Failed to turn texture to image", errno);
+		return false;		
+	}
+	tiles->txt_player = mlx_load_png("./textures/player.png");
+	if (!tiles->txt_player)
+	{
+		err_msg("Failed to load texture", errno);
+		return false;		
+	}
+	tiles->player = mlx_texture_to_image(ctx->mlx, tiles->txt_player);
+	if (!tiles->coin)
+	{
+		err_msg("Failed to turn texture to image", errno);
+		return false;		
+	}
+	ctx->map->tiles = tiles;
+	ft_printf("Loaded textures\n");
+	return true;
+}
