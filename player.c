10a8506e9ea49773cf6823eb3d4781a2c75c0426
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-19 22:50:05 +0300

    setting speed and orbs to to initial value

--- player.c
+++ player.c
@@ -9,6 +9,8 @@ bool	initialize_player(t_ctx *ctx)
 	grid = ctx->map->grid;
 	ctx->player = malloc(sizeof(t_player));
 	ctx->player->pos = malloc(sizeof(t_vec2));	
+	ctx->player->orbs = 0;
+	ctx->player->speed = 8;
 	y = -1;
 	while (grid[++y])
 	{
@@ -17,9 +19,8 @@ bool	initialize_player(t_ctx *ctx)
 		{
 			if (grid[y][x] == 'P')
 			{
-				ctx->player->pos->x = x;
-				ctx->player->pos->y = y;
-				ft_printf("pos: (%d, %d)\n", ctx->player->pos->x, ctx->player->pos->y);
+				ctx->player->pos->x = x *ctx->map->size;
+				ctx->player->pos->y = y * ctx->map->size;
 				return true;
 			}
 		}
@@ -27,7 +28,7 @@ bool	initialize_player(t_ctx *ctx)
 	return false;	
 }
 
-void	set_pod(t_vec2 *pos, int x, int y)
+void	set_pos(t_vec2 *pos, int x, int y)
 {
 	pos->x = x;
 	pos->y = y;
