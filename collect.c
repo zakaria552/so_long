4f5abebe3d9475893bcae04d643b8a709e6f562e
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-19 22:47:56 +0300

    player is now able to collect collectables

---
+++ collect.c
@@ -0,0 +1,38 @@
+#include "so_long.h"
+
+int get_instance(char **grid, t_vec2 tile, char c)
+{
+	int i;
+	int x;
+	int y;
+
+	i = x = y = -1;
+	while (grid[++y])
+	{
+		x = -1;
+		while(grid[y][++x]) 
+		{
+			if (grid[y][x] == c)
+				i++;
+			if (grid[y][x] == c && (x == tile.x && y == tile.y))
+				return i;
+		}
+	}
+	return -1;
+}
+
+void collect(t_ctx *ctx)
+{
+	t_vec2 tile;
+	int i;
+
+	tile = get_collided_tile(ctx->map, ctx->player->pos, 'C');
+	if (tile.x < 0)
+		return ;	
+	i = get_instance(ctx->map->grid, tile, 'C');
+	if (!ctx->map->tiles->coin->instances[i].enabled)
+		return;
+	ctx->map->tiles->coin->instances[i].enabled = false;
+	ctx->player->orbs += 1;
+	ft_printf("Collected: %d\n", ctx->player->orbs);
+}
