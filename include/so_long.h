5ca8427cd4915886fa4db43e5db3be129be823af
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-20 15:32:34 +0300

    added prototypes

--- include/so_long.h
+++ include/so_long.h
@@ -20,9 +20,11 @@ bool validate_map(char *fname, char *map);
 // errors
 void exit_with_err(int code);
 void err_msg(char *msg, int errnum);
+void clean_exit(t_ctx *ctx, char *msg, int err_code);
 
 // io_utils
 char *read_from_file(char *name);
+char *read_from_file2(char *name);
 
 // textures
 bool load_textures(t_ctx *ctx);
@@ -41,5 +43,9 @@ t_vec2 get_collided_tile(t_map *map, t_vec2 *pos, char c);
 
 // collect
 void collect(t_ctx *ctx);
+
+// clean
+void clean_up(t_ctx *ctx);
+void free_map(t_map *map, mlx_t *mlx);
 
 #endif
