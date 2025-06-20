03d250c3c3bff068598bfcc99066d8546dd7f8a5
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-20 15:32:54 +0300

    added clean exit func

--- errors.c
+++ errors.c
@@ -8,10 +8,19 @@ void exit_with_err(int code)
 
 void err_msg(char *msg, int errnum)
 {
-	ft_printf("Error: %s", msg);
+	ft_printf("Error: %s\n", msg);
 	if (errnum > 0)
 	{
 		errno = errnum;
 		perror(": ");
 	}
+}
+
+void clean_exit(t_ctx *ctx, char *msg, int err_code)
+{
+	errno = err_code;
+	if (msg)
+		ft_printf("Error: %s\n", msg);
+	clean_up(ctx);
+	exit(errno);
 }
