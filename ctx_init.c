5d82e0a8bd791e921797f16b02222935778561ee
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-23 17:56:20 +0300

    initializing game state

---
+++ ctx_init.c
@@ -0,0 +1,46 @@
+#include "so_long.h"
+
+t_ctx *ctx_init(char *map_name)
+{
+	t_ctx *ctx;
+
+	ctx = malloc(sizeof(t_ctx));
+	if (!ctx)
+		clean_exit(ctx, strerror(errno), errno);
+	ctx->map = NULL;
+	ctx->player = NULL;	
+	ctx->mlx = NULL;
+	ctx->state = NULL;
+	ctx->map = parse_map(map_name);
+	if (!ctx->map)
+		clean_exit(ctx, strerror(errno), errno);
+	init_game_state(ctx);
+	return ctx;
+}
+
+void init_game_state(t_ctx *ctx)
+{
+	int x;
+	int	y;
+	t_map *map;
+
+	x = y = -1;
+	map = ctx->map;
+	ctx->state = malloc(sizeof(t_game_state));
+	if (!ctx->state)
+		clean_exit(ctx, strerror(errno), errno);
+	ctx->state->collected = 0;
+	ctx->state->total_orbs = 0;
+	ctx->state->exited = false;
+	ctx->state->ready_to_exit = false;
+	while (map->grid[++y])
+	{
+		x = -1;
+		while (map->grid[y][++x])
+		{
+			if (map->grid[y][x] == 'C')
+				ctx->state->total_orbs += 1;
+		}
+	}
+}
+
