1f72e1ed06f0b3e51e5704a0330140cf090fb7b8
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-23 17:55:43 +0300

    drawing exit doors

--- draw.c
+++ draw.c
@@ -1,6 +1,7 @@
 #include "so_long.h"
 
 static void	draw_player(t_ctx *ctx);
+static void	draw_exit(t_ctx *ctx);
 
 void	draw_map(t_ctx *ctx)
 {
@@ -27,6 +28,7 @@ void	draw_map(t_ctx *ctx)
 				mlx_image_to_window(ctx->mlx, tiles->coin, x * tile_size, y * tile_size);
 		}
 	}
+	draw_exit(ctx);
 	draw_player(ctx);		
 }
 
@@ -41,4 +43,19 @@ void	draw_player(t_ctx *ctx)
 	pos = ctx->player->pos;
 	ft_printf("Drawing player (%d, %d)\n", pos->x, pos->y);
 	mlx_image_to_window(ctx->mlx, tiles->player, pos->x *ctx->map->size, pos->y* ctx->map->size);
+}
+
+void	draw_exit(t_ctx *ctx)
+{
+	t_tiles *tiles;
+	int tile_size;
+	t_vec2 *exit;
+	
+	tiles = ctx->map->tiles;
+	tile_size = ctx->map->size;
+	exit = ctx->map->exit;
+	ft_printf("Drawing exit (%d, %d)\n", exit->x, exit->y);
+	mlx_image_to_window(ctx->mlx, tiles->exit, exit->x * ctx->map->size, exit->y * ctx->map->size);	
+	mlx_image_to_window(ctx->mlx, tiles->exit_door, exit->x * ctx->map->size, exit->y * ctx->map->size);	
+	tiles->exit->instances[0].enabled = false;
 }
