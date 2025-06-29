1dc91cc5826a34e1c35ef8081a3002d7c381289a
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-29 17:26:41 +0300

    added update vision func that restricts player's vision

--- player.c
+++ player.c
@@ -1,30 +1,50 @@
 #include "so_long.h"
 
-static bool init_player_pos(t_ctx *ctx);
+static void init_player_pos(t_ctx *ctx);
+static void set_pixel(const mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color);
+static uint32_t obaque_by_d(int d, int r);
 
-bool	initialize_player(t_ctx *ctx)
+void initialize_player(t_ctx *ctx)
 {
 	ctx->player = malloc(sizeof(t_player));
 	if (!ctx->player)
-		return false;
+		clean_exit(ctx, NULL, errno);
 	ctx->player->pos = malloc(sizeof(t_vec2));	
 	if (!ctx->player->pos)
-	{
-		free(ctx->player);
-		return NULL;
-	}
+		clean_exit(ctx, NULL, errno);
+	ctx->player->vision = malloc(sizeof(t_vision));
+	if(!ctx->player->vision)
+		clean_exit(ctx, NULL, errno);
 	ctx->player->orbs = 0;
 	ctx->player->speed = 8;
-	return init_player_pos(ctx);	
+	ctx->player->vision->r = 150;
+	init_player_pos(ctx);
 }
 
-void	set_pos(t_vec2 *pos, int x, int y)
+void	update_vision(t_ctx *ctx)
 {
-	pos->x = x;
-	pos->y = y;
+	const t_vec2 *pos = ctx->player->pos;
+	const mlx_image_t *img = ctx->player->vision->img;
+	const int r = ctx->player->vision->r;
+	int x;
+	int y;
+	int d;
+
+	y = 0;
+	x = 0;
+	while (y++ < img->height)
+	{
+		x = -1;
+		while (++x < img->width)
+		{
+			d = (x - (int)(pos->x + 24)) * (x - (int)(pos->x + 24)) +
+			(y - (int)(pos->y + 24)) * (y - (int)(pos->y + 24));
+			set_pixel(img, x, y, obaque_by_d(d, r * r));
+		}
+	}
 }
 
-static bool init_player_pos(t_ctx *ctx)
+static void init_player_pos(t_ctx *ctx)
 {
 	char **grid;
 	int x;
@@ -41,9 +61,33 @@ static bool init_player_pos(t_ctx *ctx)
 			{
 				ctx->player->pos->x = x;
 				ctx->player->pos->y = y;
-				return true;
+				return ;
 			}
 		}
 	}
-	return false;
+}
+
+static void set_pixel(const mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color)
+{
+    const uint32_t index = (y * img->width + x) * 4;
+
+    if (x >= img->width || y >= img->height)
+        return;
+    img->pixels[index + 0] = (color >> 24) & 0xFF;
+    img->pixels[index + 1] = (color >> 16) & 0xFF;
+    img->pixels[index + 2] = (color >> 8)  & 0xFF;
+    img->pixels[index + 3] = (color >> 0)  & 0xFF;
+}
+
+static uint32_t obaque_by_d(int d, int r)
+{
+	uint32_t c;
+	float a;
+
+	c = 0x000000ff;
+	a = (float) d / r;
+	a *= 0xff;
+	if (d < r)
+		c = 0x00000000 | (int) a;
+	return c;
 }
