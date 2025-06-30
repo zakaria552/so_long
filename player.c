32dc463d3239e9c779517bef62fcaf6c9fd827e1
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-30 16:19:14 +0300

    removed logics for the vision

--- player.c
+++ player.c
@@ -1,8 +1,6 @@
 #include "so_long.h"
 
 static void init_player_pos(t_ctx *ctx);
-static void set_pixel(const mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color);
-static uint32_t obaque_by_d(int d, int r);
 
 void initialize_player(t_ctx *ctx)
 {
@@ -19,29 +17,6 @@ void initialize_player(t_ctx *ctx)
 	ctx->player->speed = 8;
 	ctx->player->vision->r = 150;
 	init_player_pos(ctx);
-}
-
-void	update_vision(t_ctx *ctx)
-{
-	const t_vec2 *pos = ctx->player->pos;
-	const mlx_image_t *img = ctx->player->vision->img;
-	const int r = ctx->player->vision->r;
-	int x;
-	int y;
-	int d;
-
-	y = 0;
-	x = 0;
-	while (y++ < img->height)
-	{
-		x = -1;
-		while (++x < img->width)
-		{
-			d = (x - (int)(pos->x + 24)) * (x - (int)(pos->x + 24)) +
-			(y - (int)(pos->y + 24)) * (y - (int)(pos->y + 24));
-			set_pixel(img, x, y, obaque_by_d(d, r * r));
-		}
-	}
 }
 
 static void init_player_pos(t_ctx *ctx)
@@ -67,27 +42,3 @@ static void init_player_pos(t_ctx *ctx)
 	}
 }
 
-static void set_pixel(const mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color)
-{
-    const uint32_t index = (y * img->width + x) * 4;
-
-    if (x >= img->width || y >= img->height)
-        return;
-    img->pixels[index + 0] = (color >> 24) & 0xFF;
-    img->pixels[index + 1] = (color >> 16) & 0xFF;
-    img->pixels[index + 2] = (color >> 8)  & 0xFF;
-    img->pixels[index + 3] = (color >> 0)  & 0xFF;
-}
-
-static uint32_t obaque_by_d(int d, int r)
-{
-	uint32_t c;
-	float a;
-
-	c = 0x000000ff;
-	a = (float) d / r;
-	a *= 0xff;
-	if (d < r)
-		c = 0x00000000 | (int) a;
-	return c;
-}
