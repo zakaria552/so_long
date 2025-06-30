a5183e40062eb8f27fb10538215c102bf337ac08
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-30 22:32:45 +0300

    splitted up the funcs into multiple files

--- textures.c
+++ textures.c
@@ -1,9 +1,7 @@
 #include "so_long.h"
 
-static void load_wall_textures(t_ctx *ctx, t_tiles *tiles);
-static void load_floor_textures(t_ctx *ctx, t_tiles *tiles);
-static void load_doors_textures(t_ctx *ctx, t_tiles *tiles);
 static void load_player_textures(t_ctx *ctx, t_tiles *tiles);
+static void init_textures(t_tiles *tiles);
 
 void load_textures(t_ctx *ctx)
 {
@@ -13,82 +11,24 @@ void load_textures(t_ctx *ctx)
 	if (!tiles)
 		clean_exit(ctx, NULL, errno);
 	ctx->map->tiles = tiles;
+	init_textures(tiles);
 	load_wall_textures(ctx, tiles);
 	load_floor_textures(ctx, tiles);
 	load_doors_textures(ctx, tiles);
 	load_player_textures(ctx, tiles);
-	tiles->txt_coin = mlx_load_png("./textures/coin.png");
-	if (!tiles->txt_coin)
-		clean_exit(ctx, "Failed to load texture", errno);
-	tiles->coin = mlx_texture_to_image(ctx->mlx, tiles->txt_coin);
-	if (!tiles->coin)
-		clean_exit(ctx, "Failed to turn texture to image", errno);
-	mlx_resize_image(tiles->coin, 64, 64);
+	load_orbs_textures(ctx, tiles);
 	ft_printf("Loaded textures\n");
 }
 
-static void load_wall_textures(t_ctx *ctx, t_tiles *tiles)
+void init_assets(t_asset *asset, int size)
 {
-	const char *src[] = {"./textures/map/wall.png", "textures/map/wall_shadow.png", NULL};
 	int i;
 
-	tiles->walls = malloc(sizeof(t_asset) * 2);
-	if (!tiles->walls)
-		clean_exit(ctx, "Failed to load assets", errno);
 	i = -1;
-	while (src[++i])
+	while (++i < size)
 	{
-		tiles->walls[i].txt = mlx_load_png(src[i]);
-		if (!tiles->walls[i].txt)
-			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->walls[i].img = mlx_texture_to_image(ctx->mlx, tiles->walls[i].txt);
-		if (!tiles->walls[i].img)
-			clean_exit(ctx, "Failed to load assets", errno);
-		mlx_resize_image(tiles->walls[i].img, ctx->map->size, ctx->map->size);
-	}
-}
-
-static void load_floor_textures(t_ctx *ctx, t_tiles *tiles)
-{
-	const char *src[] = {"./textures/map/tile1.png", "textures/map/tile2.png", 
-		"textures/map/tile3.png", "textures/map/tile4.png", NULL};
-
-	int i;
-
-	tiles->floors = malloc(sizeof(t_asset) * 4);
-	if (!tiles->floors)
-		clean_exit(ctx, "Failed to load assets", errno);
-	i = -1;
-	while (src[++i])
-	{
-		tiles->floors[i].txt = mlx_load_png(src[i]);
-		if (!tiles->floors[i].txt)
-			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->floors[i].img = mlx_texture_to_image(ctx->mlx, tiles->floors[i].txt);
-		if (!tiles->floors[i].img)
-			clean_exit(ctx, "Failed to load assets", errno);
-		mlx_resize_image(tiles->floors[i].img, ctx->map->size, ctx->map->size);
-	}
-}
-static void load_doors_textures(t_ctx *ctx, t_tiles *tiles)
-{
-	const char *src[] = {"./textures/map/closed.png", "textures/map/open.png", NULL};
-
-	int i;
-
-	tiles->doors = malloc(sizeof(t_asset) * 2);
-	if (!tiles->doors)
-		clean_exit(ctx, "Failed to load assets", errno);
-	i = -1;
-	while (src[++i])
-	{
-		tiles->doors[i].txt = mlx_load_png(src[i]);
-		if (!tiles->doors[i].txt)
-			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->doors[i].img = mlx_texture_to_image(ctx->mlx, tiles->doors[i].txt);
-		if (!tiles->doors[i].img)
-			clean_exit(ctx, "Failed to load assets", errno);
-		mlx_resize_image(tiles->doors[i].img, ctx->map->size, ctx->map->size);
+		asset[i].img = NULL;
+		asset[i].txt = NULL;
 	}
 }
 static void load_player_textures(t_ctx *ctx, t_tiles *tiles)
@@ -102,6 +42,7 @@ static void load_player_textures(t_ctx *ctx, t_tiles *tiles)
 		clean_exit(ctx, "Failed to load assets", errno);
 	ft_printf("Reached here");
 	i = -1;
+	init_assets(tiles->p_idle, 1);
 	while (src[++i])
 	{
 		tiles->p_idle[i].txt = mlx_load_png(src[i]);
@@ -113,4 +54,12 @@ static void load_player_textures(t_ctx *ctx, t_tiles *tiles)
 		mlx_resize_image(tiles->p_idle[i].img, 48, 48);
 	}
 
+}
+static void init_textures(t_tiles *tiles)
+{
+	tiles->doors = NULL;
+	tiles->floors = NULL;
+	tiles->orbs = NULL;
+	tiles->walls = NULL;
+	tiles->p_idle = NULL;
 }
