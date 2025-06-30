4e39ef9eac2de321bca96f99fd3997430fbce700
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-06-30 16:21:17 +0300

    added generic move hook

--- player_mov_hook.c
+++ player_mov_hook.c
@@ -1,34 +1,29 @@
 #include "so_long.h"
 static void move_player(t_ctx *ctx, int nx, int ny);
 
-void handle_player_movement(mlx_key_data_t keydata, t_ctx *ctx)
+void move_hook(t_ctx *ctx)
 {
-	t_vec2 *pos;
-	
-	if (ctx->state->exited)
-		return;
-	//ft_printf("Player moving\n");
-	pos = ctx->player->pos;
-	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
+	const t_vec2 *pos = ctx->player->pos;
+
+	if (mlx_is_key_down(ctx->mlx, MLX_KEY_W))
 		move_player(ctx, pos->x, pos->y - 8);
-	if (keydata.key == MLX_KEY_S && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
+	if (mlx_is_key_down(ctx->mlx, MLX_KEY_S))
 		move_player(ctx, pos->x, pos->y + 8);
-	if (keydata.key == MLX_KEY_A && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
+	if (mlx_is_key_down(ctx->mlx, MLX_KEY_A))
 		move_player(ctx, pos->x - 8, pos->y);
-	if (keydata.key == MLX_KEY_D && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
+	if (mlx_is_key_down(ctx->mlx, MLX_KEY_D))
 		move_player(ctx, pos->x + 8, pos->y);
-	if (keydata.key == MLX_KEY_ESCAPE)
-		mlx_close_window(ctx->mlx);
-		
 }
+
 void move_player(t_ctx *ctx, int nx, int ny)
 {
 	mlx_image_t *img;
+	const offsets[2][2] = {{10, 10}, {0, 36}};
 
 	img = ctx->map->tiles->p_idle->img;	
 	ctx->player->pos->x = nx;
 	ctx->player->pos->y = ny;
-	if (check_collision(ctx, '1'))
+	if (check_collision(ctx, '1', offsets))
 	{
 		ctx->player->pos->x = img->instances[0].x;
 		ctx->player->pos->y = img->instances[0].y;
