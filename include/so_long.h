41d94271344a8833c25575dd1f649fec3700193d
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-01 17:34:54 +0300

    norm fixes

--- include/so_long.h
+++ include/so_long.h
@@ -1,50 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:33:42 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/01 17:33:43 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
 // #include <mlx.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include "libft.h"
-#include "so_types.h"
-#include "MLX42.h"
-#include "math.h"
+# include "MLX42.h"
+# include "libft.h"
+# include "math.h"
+# include "so_types.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
 
 // parser
-t_map *parse_map(char *name);
+t_map	*parse_map(char *name);
 
 // ctx init
-t_ctx *ctx_init(char *map_name);
-void init_game_state(t_ctx *ctx);
+t_ctx	*ctx_init(char *map_name);
 
-// map validator
-bool validate_map(char *fname, char *map);
+// map validator/utils
+bool	validate_map(char *fname, char *map);
+bool	valid_char_set(char **map);
+bool	is_rectangular(char **map);
+bool	has_extension(char *name, char *ext);
+bool	is_enclosed(char **map);
 
 // errors
-void exit_with_err(int code);
-void err_msg(char *msg, int errnum);
-void clean_exit(t_ctx *ctx, char *msg, int err_code);
+void	err_msg(char *msg, int errnum);
+void	clean_exit(t_ctx *ctx, char *msg, int err_code);
 
 // io_utils
-char *read_from_file(char *name);
-char *read_from_file2(char *name);
+char	*read_from_file(char *name);
+char	*read_from_file2(char *name);
 
 // textures
-void load_textures(t_ctx *ctx);
-void init_assets(t_asset *asset, int size);
+void	load_textures(t_ctx *ctx);
+void	init_assets(t_asset *asset, int size);
 
 // textures utils
-void load_wall_textures(t_ctx *ctx, t_tiles *tiles);
-void load_floor_textures(t_ctx *ctx, t_tiles *tiles);
-void load_doors_textures(t_ctx *ctx, t_tiles *tiles);
-void load_orbs_textures(t_ctx *ctx, t_tiles *tiles);
+void	load_wall_textures(t_ctx *ctx, t_tiles *tiles);
+void	load_floor_textures(t_ctx *ctx, t_tiles *tiles);
+void	load_doors_textures(t_ctx *ctx, t_tiles *tiles);
+void	load_orbs_textures(t_ctx *ctx, t_tiles *tiles);
 
 // draw
 void	draw_map(t_ctx *ctx);
 
 // player
-void move_hook(t_ctx *ctx);
+void	move_hook(t_ctx *ctx);
 void	initialize_player(t_ctx *ctx);
 
 // vision
@@ -52,31 +66,32 @@ void	update_vision(t_ctx *ctx);
 void	init_vision(t_ctx *ctx);
 
 // collision
-bool check_collision(t_ctx *ctx, char c, const off[2][2]);
-t_vec2 get_collided_tile(t_map *map, t_vec2 *pos, char c);
+bool	check_collision(t_ctx *ctx, char c, const int off[2][2]);
+t_vec2	get_collided_tile(t_map *map, t_vec2 *pos, char c);
 
 // collect
-void collect(t_ctx *ctx);
+void	collect(t_ctx *ctx);
 
-// clean
-void clean_up(t_ctx *ctx);
-void free_map(t_map *map, mlx_t *mlx);
+// clean/utils
+void	clean_up(t_ctx *ctx);
+void	free_map(t_map *map, mlx_t *mlx);
 void	*free_matrix_mem(char **matrix);
 void	free_tiles(mlx_t *mlx, t_tiles *tiles);
+void	free_player(t_ctx *ctx, t_player *player);
+void	free_assets(mlx_t *mlx, t_asset *asset, int size);
 
 // valid path
-t_list *dfs_target(t_map *map, int bounds[2], t_vec2 start, t_vec2 target);
-bool valid_path_exists(t_map *map, t_player *player);
+t_list	*dfs_target(t_map *map, int bounds[2], t_vec2 start, t_vec2 target);
+bool	valid_path_exists(t_map *map, t_player *player);
 
 // valid path utils
-void print_visit(int **visited, int w, int h);
-bool is_valid_adj(t_list **q, t_vec2 *adj, int bounds[2], int **visited);
-int **initialize_visited(int w, int h);
-void show_path(t_list *p);
-void **free_visited(int **visited, int w, int h);
+void	print_visit(int **visited, int w, int h);
+bool	is_valid_adj(t_list **q, t_vec2 *adj, int bounds[2], int **visited);
+int		**initialize_visited(int w, int h);
+void	show_path(t_list *p);
+void	free_visited(int **visited, int w, int h);
 
 // path finder
-t_list *dfs_target(t_map *map, int bounds[2], t_vec2 start, t_vec2 target);
-
+t_list	*dfs_target(t_map *map, int bounds[2], t_vec2 start, t_vec2 target);
 
 #endif
--- include/so_types.h
+++ include/so_types.h
@@ -1,83 +1,96 @@
-#ifndef SO_TYPES_H
-#define SO_TYPES_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_types.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:34:14 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/01 17:34:40 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "MLX42.h"
+#ifndef SO_TYPES_H
+# define SO_TYPES_H
+
+# include "MLX42.h"
+
 typedef struct s_vec2
 {
-	int x;
-	int y;
-} t_vec2;
+	int				x;
+	int				y;
+}					t_vec2;
 
 typedef struct s_vision
 {
-	mlx_image_t *img;
-	int r;
-} t_vision;
+	mlx_image_t		*img;
+	int				r;
+}					t_vision;
 
 typedef struct s_player
 {
-	t_vec2 *pos;
-	int orbs;
-	int speed;
-	t_vision *vision;
-} t_player;
+	t_vec2			*pos;
+	int				orbs;
+	int				speed;
+	t_vision		*vision;
+}					t_player;
 
 typedef struct s_orb
 {
-	t_vec2 *pos;
-	bool free;
-} t_orb;
+	t_vec2			*pos;
+	bool			free;
+}					t_orb;
 
-typedef struct s_textures 
+typedef struct s_textures
 {
-	mlx_texture_t *empty_tile;
-} t_textures;
+	mlx_texture_t	*empty_tile;
+}					t_textures;
 
 typedef struct s_asset
 {
-	mlx_texture_t *txt;
-	mlx_image_t *img;
-} t_asset;
+	mlx_texture_t	*txt;
+	mlx_image_t		*img;
+}					t_asset;
 
 typedef struct s_tiles
 {
-	t_asset *walls;
-	t_asset *floors;
-	t_asset *doors;
-	t_asset *p_idle;
-	t_asset *orbs;
-} t_tiles;
+	t_asset			*walls;
+	t_asset			*floors;
+	t_asset			*doors;
+	t_asset			*p_idle;
+	t_asset			*orbs;
+}					t_tiles;
 
 typedef struct s_map
 {
-	char **grid;
-	char **init_grid;
-	int width;
-	int height;
-	int bounds[2];
-	int size;
-	t_tiles *tiles;
-	t_vec2 *start;
-	t_vec2 *exit;
-	t_orb *orbs;
-} t_map;
+	char			**grid;
+	char			**init_grid;
+	int				width;
+	int				height;
+	int				bounds[2];
+	int				size;
+	t_tiles			*tiles;
+	t_vec2			*start;
+	t_vec2			*exit;
+	t_orb			*orbs;
+}					t_map;
 
 typedef struct s_game_state
 {
-	int total_orbs;
-	int collected;
-	bool ready_to_exit;
-	bool exited;
-} t_game_state;
+	int				total_orbs;
+	int				collected;
+	bool			ready_to_exit;
+	bool			exited;
+}					t_game_state;
 
 typedef struct s_ctx
 {
-	mlx_t *mlx;
-	t_player *player;
-	t_player *enemies;
-	t_map *map;
-	t_game_state *state;
+	mlx_t			*mlx;
+	t_player		*player;
+	t_player		*enemies;
+	t_map			*map;
+	t_game_state	*state;
 
-} t_ctx;
+}					t_ctx;
 
 #endif
