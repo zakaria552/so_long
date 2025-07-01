2a6f797faa0b76e548189f1c15c64392a97262c1
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-01 20:22:57 +0300

    handling errors

--- src/draw.c
+++ src/draw.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/01 14:43:54 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/01 17:05:39 by zfarah           ###   ########.fr       */
+/*   Created: 2025/07/01 18:19:52 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/01 18:27:29 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,8 @@
 
 static void	draw_player(t_ctx *ctx);
 static void	draw_exit(t_ctx *ctx);
-
-void	draw_map_borders(t_ctx *ctx, t_asset *walls, int size)
-{
-	int	x;
-	int	y;
-
-	x = 0;
-	y = -1;
-	while (++y < ctx->map->bounds[1] - 1)
-	{
-		mlx_image_to_window(ctx->mlx, walls[0].img, x * size, y * size);
-		mlx_image_to_window(ctx->mlx, walls[0].img, (ctx->map->bounds[0] - 1)
-			* size, y * size);
-	}
-	y = 0;
-	x = 0;
-	while (++x < ctx->map->bounds[0] - 1)
-	{
-		mlx_image_to_window(ctx->mlx, walls[1].img, x * size, y * size);
-		mlx_image_to_window(ctx->mlx, walls[1].img, x * size,
-			(ctx->map->bounds[1] - 1) * size);
-	}
-	mlx_image_to_window(ctx->mlx, walls[1].img, 0 * size, (ctx->map->bounds[1]
-			- 1) * size);
-	mlx_image_to_window(ctx->mlx, walls[1].img, (ctx->map->bounds[0] - 1)
-		* size, (ctx->map->bounds[1] - 1) * size);
-}
-
-void	draw_map_grid(t_ctx *ctx, t_map *map)
-{
-	const char		**grid = (const char **)map->grid;
-	const t_tiles	*tiles = map->tiles;
-	int				x;
-	int				y;
-
-	y = 0;
-	while (++y < map->bounds[1] - 1)
-	{
-		x = 0;
-		while (++x < ctx->map->bounds[0] - 1)
-		{
-			if (grid[y][x] == '1' && grid[y + 1][x] != '1')
-				mlx_image_to_window(ctx->mlx, tiles->walls[1].img, x
-					* map->size, y * map->size);
-			else if (grid[y][x] == '1' && grid[y + 1][x] == '1')
-				mlx_image_to_window(ctx->mlx, tiles->walls[0].img, x
-					* map->size, y * map->size);
-			else
-				mlx_image_to_window(ctx->mlx, tiles->floors[(x * y) % 4].img, x
-					* map->size, y * map->size);
-			if (grid[y][x] == 'C')
-				mlx_image_to_window(ctx->mlx, tiles->orbs->img, x * map->size, y
-					* map->size);
-		}
-	}
-}
+static void	draw_map_borders(t_ctx *ctx, t_asset *walls, int size);
+static void	draw_map_grid(t_ctx *ctx, t_map *map);
 
 void	draw_map(t_ctx *ctx)
 {
@@ -87,26 +33,81 @@ void	draw_map(t_ctx *ctx)
 	draw_player(ctx);
 }
 
-void	draw_player(t_ctx *ctx)
+static void	draw_map_borders(t_ctx *ctx, t_asset *walls, int size)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = -1;
+	while (++y < ctx->map->bounds[1] - 1)
+	{
+		img_to_window(ctx, walls[0].img, x * size, y * size);
+		img_to_window(ctx, walls[0].img, (ctx->map->bounds[0] - 1) * size, y
+			* size);
+	}
+	y = 0;
+	x = 0;
+	while (++x < ctx->map->bounds[0] - 1)
+	{
+		img_to_window(ctx, walls[1].img, x * size, y * size);
+		img_to_window(ctx, walls[1].img, x * size, (ctx->map->bounds[1] - 1)
+			* size);
+	}
+	img_to_window(ctx, walls[1].img, 0 * size, (ctx->map->bounds[1] - 1)
+		* size);
+	img_to_window(ctx, walls[1].img, (ctx->map->bounds[0] - 1) * size,
+		(ctx->map->bounds[1] - 1) * size);
+}
+
+static void	draw_map_grid(t_ctx *ctx, t_map *map)
+{
+	const char		**grid = (const char **)map->grid;
+	const t_tiles	*tiles = map->tiles;
+	int				x;
+	int				y;
+
+	y = 0;
+	while (++y < map->bounds[1] - 1)
+	{
+		x = 0;
+		while (++x < ctx->map->bounds[0] - 1)
+		{
+			if (grid[y][x] == '1' && grid[y + 1][x] != '1')
+				img_to_window(ctx, tiles->walls[1].img, x * map->size, y
+					* map->size);
+			else if (grid[y][x] == '1' && grid[y + 1][x] == '1')
+				img_to_window(ctx, tiles->walls[0].img, x * map->size, y
+					* map->size);
+			else
+				img_to_window(ctx, tiles->floors[(x * y) % 4].img, x
+					* map->size, y * map->size);
+			if (grid[y][x] == 'C')
+				img_to_window(ctx, tiles->orbs->img, x * map->size, y
+					* map->size);
+		}
+	}
+}
+
+static void	draw_player(t_ctx *ctx)
 {
 	t_tiles	*tiles;
 	int		tile_size;
 	t_vec2	*pos;
 
-	// handle failure
 	tiles = ctx->map->tiles;
 	tile_size = ctx->map->size;
 	pos = ctx->player->pos;
 	pos->x *= ctx->map->size;
 	pos->y *= ctx->map->size;
-	ft_printf("Drawing player (%d, %d)\n", pos->x, pos->y);
-	ctx->player->vision->img = mlx_new_image(ctx->mlx, ctx->map->width,
-			ctx->map->height);
-	mlx_image_to_window(ctx->mlx, ctx->player->vision->img, 0, 0);
-	mlx_image_to_window(ctx->mlx, tiles->p_idle[0].img, pos->x, pos->y);
+	ctx->player->vision->img = new_img(ctx, ctx->map->width, ctx->map->height);
+	img_to_window(ctx, ctx->player->vision->img, 0, 0);
+	img_to_window(ctx, tiles->p_idle[0].img, pos->x, pos->y);
+	if (ctx->player->vision->img < 0 || tiles->p_idle[0].img < 0)
+		clean_exit(ctx, NULL, errno);
 }
 
-void	draw_exit(t_ctx *ctx)
+static void	draw_exit(t_ctx *ctx)
 {
 	t_tiles	*tiles;
 	int		tile_size;
@@ -115,10 +116,11 @@ void	draw_exit(t_ctx *ctx)
 	tiles = ctx->map->tiles;
 	tile_size = ctx->map->size;
 	exit = ctx->map->exit;
-	ft_printf("Drawing exit (%d, %d)\n", exit->x, exit->y);
-	mlx_image_to_window(ctx->mlx, tiles->doors[1].img, exit->x * ctx->map->size,
+	img_to_window(ctx, tiles->doors[1].img, exit->x * ctx->map->size,
 		exit->y * ctx->map->size);
-	mlx_image_to_window(ctx->mlx, tiles->doors[0].img, exit->x * ctx->map->size,
+	img_to_window(ctx, tiles->doors[0].img, exit->x * ctx->map->size,
 		exit->y * ctx->map->size);
+	if (tiles->doors[0].img < 0 || tiles->doors[1].img < 0)
+		clean_exit(ctx, NULL, errno);
 	tiles->doors[1].img->instances[0].enabled = false;
 }
