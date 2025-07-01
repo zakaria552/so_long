dfc1dedbf54325addaf78cf299ad78dd2a204938
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-01 20:27:19 +0300

    removed printfs

--- src/map_validator.c
+++ src/map_validator.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 14:50:24 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/01 15:37:23 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/01 18:32:24 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,16 @@ bool	validate_map(char *fname, char *map)
 		return (false);
 	if (!valid_char_set(split))
 	{
-		ft_printf("Contains/missing char set");
 		free_matrix_mem(split);
 		return (false);
 	}
 	if (!is_rectangular(split))
 	{
-		ft_printf("Not rectangular\n");
 		free_matrix_mem(split);
 		return (false);
 	}
 	if (!is_enclosed(split))
 	{
-		ft_printf("Not enclosed\n");
 		free_matrix_mem(split);
 		return (false);
 	}
