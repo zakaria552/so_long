ca62fa15eba6f4aba290cf1ee395bfdabe1b7914
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-01 20:28:39 +0300

    refactored

--- src/valid_path.c
+++ src/valid_path.c
@@ -6,40 +6,43 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 15:53:46 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/01 15:54:05 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/01 19:30:59 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static t_vec2	target_vec(int x, int y);
+
 bool	valid_path_exists(t_map *map, t_player *player)
 {
 	t_list	*path;
-	t_vec2	target;
 	int		x;
 	int		y;
 
-	x = y = -1;
+	y = -1;
 	while (map->grid[++y])
 	{
 		x = -1;
 		while (map->grid[y][++x])
 		{
-			if (map->grid[y][x] != '1' && map->grid[y][x] != '0'
-				&& map->grid[y][x] != 'P')
-			{
-				target.x = x;
-				target.y = y;
-				path = dfs_target(map, map->bounds, *player->pos, target);
-				if (!path)
-				{
-					ft_lstclear(&path, free);
-					return (false);
-				}
-				ft_lstclear(&path, free);
-			}
+			if (map->grid[y][x] == '1' || map->grid[y][x] == '0'
+				|| map->grid[y][x] == 'P')
+				continue ;
+			path = dfs_target(map, map->bounds, *player->pos, target_vec(x, y));
+			if (!path)
+				return (false);
+			ft_lstclear(&path, free);
 		}
 	}
-	ft_lstclear(&path, free);
 	return (true);
+}
+
+static t_vec2	target_vec(int x, int y)
+{
+	t_vec2	target;
+
+	target.x = x;
+	target.y = y;
+	return (target);
 }
