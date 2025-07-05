06973c0ad1ef056c9f1b11bca4ee3c3edbf40684
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-05 23:44:52 +0300

    wip bonus include

---
+++ includeb/so_types.h
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_types.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:34:14 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/05 22:35:30 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_TYPES_H
+# define SO_TYPES_H
+
+# include "MLX42.h"
+
+typedef struct s_vec2
+{
+	int				x;
+	int				y;
+}					t_vec2;
+
+typedef struct s_vision
+{
+	mlx_image_t		*img;
+	int				r;
+}					t_vision;
+
+typedef enum e_dir
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	IDLE,
+} t_dir;
+
+typedef struct s_animation_info
+{
+	int frame_index;
+	double	lt;
+	int frame;
+} t_animation_info;
+
+typedef struct s_player
+{
+	t_vec2			*pos;
+	int				orbs;
+	int				speed;
+	t_dir			prev_dir;
+	t_dir			dir;
+	bool			idle;
+	t_vision		*vision;
+	t_animation_info *ani_info;
+}					t_player;
+
+typedef struct s_orb
+{
+	t_vec2			*pos;
+	bool			free;
+}					t_orb;
+
+typedef struct s_textures
+{
+	mlx_texture_t	*empty_tile;
+}					t_textures;
+
+typedef struct s_asset
+{
+	mlx_texture_t	*txt;
+	mlx_image_t		*img;
+}					t_asset;
+
+typedef struct s_tiles
+{
+	t_asset			*walls;
+	t_asset			*floors;
+	t_asset			*doors;
+	t_asset			*p_idle;
+	t_asset			*orbs;
+	t_asset			*idle;
+	t_asset			*right;
+	t_asset			*left;
+	t_asset			*up;
+	t_asset			*down;
+}					t_tiles;
+
+typedef struct s_map
+{
+	char			**grid;
+	char			**init_grid;
+	int				width;
+	int				height;
+	int				bounds[2];
+	int				size;
+	t_tiles			*tiles;
+	t_vec2			*start;
+	t_vec2			*exit;
+	t_orb			*orbs;
+}					t_map;
+
+typedef struct s_game_state
+{
+	int				total_orbs;
+	int				collected;
+	bool			ready_to_exit;
+	bool			exited;
+}					t_game_state;
+
+typedef struct s_ctx
+{
+	mlx_t			*mlx;
+	t_player		*player;
+	t_player		*enemies;
+	t_map			*map;
+	t_game_state	*state;
+
+}					t_ctx;
+
+#endif
