9747e9b6f1433c93a08dce79f10e4b0d18db2829
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-09 19:34:55 +0300

    removed player animation logic

--- srcb/main.c
+++ srcb/main.c
@@ -13,61 +13,6 @@
 #include "so_long.h"
 #include <stdio.h>
 
-char *dir(t_dir dir)
-{
-	if (dir == IDLE)
-		return "idle";
-	else if (dir == RIGHT)
-		return "right";
-	else if (dir == LEFT)
-		return "left";
-	else if (dir == UP)
-		return "up";
-	else if (dir == DOWN)
-		return "down";
-}
-void disable_images(t_ctx *ctx, int i)
-{
-	if (ctx->player->dir != IDLE)
-		ctx->map->tiles->idle[i].img->enabled = false;
-	if (ctx->player->dir != RIGHT)
-		ctx->map->tiles->right[i].img->enabled = false;
-	if (ctx->player->dir != LEFT)
-		ctx->map->tiles->left[i].img->enabled = false;
-	if (ctx->player->dir != DOWN)
-		ctx->map->tiles->down[i].img->enabled = false;
-	if (ctx->player->dir != UP)
-		ctx->map->tiles->up[i].img->enabled = false;
-}
-void	animate(t_ctx *ctx)
-{
-	int i;
-	t_animation_info *info = ctx->player->ani_info;
-	t_asset	*img = get_img_from_dir(ctx, ctx->player->dir);
-	double dt = mlx_get_time() - info->lt;
-	i = -1;
-	//printf("lt: %.2f, dt: %.2f, fi: %d\n", info->lt, dt, info->frame_index);
-	//ft_printf("Dir: %s", dir(ctx->player->dir));
-	if ((dt > 0.07 && ctx->player->dir != IDLE) || dt > 0.25)
-	{
-		ft_printf("Moving to next frame....\n");
-		ft_printf("frame: %d, fi: %d\n", ctx->player->ani_info->frame, ctx->player->ani_info->frame_index);
-		while (++i < 5)
-		{
-			ft_printf("i: %d\n", i);
-			if ((i == info->frame_index))
-				info->frame = i;
-			img[i].img->enabled = (i == info->frame_index);
-			disable_images(ctx, i);
-		}
-		info->frame_index += 1;
-		if (info->frame_index == 5)
-			ctx->player->ani_info->frame_index = 0;
-		info->lt = mlx_get_time();
-	}
-
-}
-
 int	main(int argc, char **args)
 {
 	t_ctx	*ctx;
@@ -86,13 +31,13 @@ int	main(int argc, char **args)
 	load_textures(ctx);
 	draw_map(ctx);
 	mlx_loop_hook(ctx->mlx, patrol, ctx);
-	mlx_loop_hook(ctx->mlx, animate, ctx);
+	mlx_loop_hook(ctx->mlx, animate_player, ctx);
 	mlx_loop_hook(ctx->mlx, move_hook, ctx);
 	mlx_loop_hook(ctx->mlx, hooks, ctx);
 	mlx_loop_hook(ctx->mlx, update_vision, ctx);
 	mlx_key_hook(ctx->mlx, key_hooks, ctx);
 	mlx_loop(ctx->mlx);
-	ft_printf("Freeing....");
+	// ft_printf("Freeing....");
 	clean_up(ctx);
 	return (0);
 }
