3a7a912b85f028fbe3052c12e966cf4618b108a7
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-09 20:51:33 +0300

    initializing enemy sprites

--- srcb/enemy_textures.c
+++ srcb/enemy_textures.c
@@ -7,4 +7,9 @@ void	load_enemy_textures(t_ctx *ctx)
 	load_enemy_left(ctx);
 	load_enemy_right(ctx);
 	load_enemy_up(ctx);
+	ctx->enemy->sprites[0] = ctx->enemy->up;
+	ctx->enemy->sprites[1] = ctx->enemy->down;
+	ctx->enemy->sprites[2] = ctx->enemy->left;
+	ctx->enemy->sprites[3] = ctx->enemy->right;
+	ctx->enemy->sprites[4] = ctx->enemy->idle;
 }
