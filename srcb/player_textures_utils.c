6594ccd41334a9f2495a753a8c8ea50204dcea24
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-09 21:35:57 +0300

    norm fixes

--- srcb/player_textures_utils.c
+++ srcb/player_textures_utils.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_textures_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 21:34:45 by zfarah            #+#    #+#             */
+/*   Updated: 2025/07/09 21:35:38 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 void	load_idle(t_ctx *ctx, t_tiles *tiles)
 {
-	const char	*src[] = {"./textures/player/idle/1.png", "./textures/player/idle/2.png",
-		"./textures/player/idle/3.png", "./textures/player/idle/4.png", "./textures/player/idle/5.png",NULL};
+	const char	*src[] = {"./textures/player/idle/1.png",
+		"./textures/player/idle/2.png", "./textures/player/idle/3.png",
+		"./textures/player/idle/4.png", "./textures/player/idle/5.png",
+		NULL};
 	int			i;
 
 	tiles->idle = malloc(sizeof(t_asset) * 5);
@@ -17,19 +31,20 @@ void	load_idle(t_ctx *ctx, t_tiles *tiles)
 		tiles->idle[i].txt = mlx_load_png(src[i]);
 		if (!tiles->idle[i].txt)
 			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->idle[i].img = mlx_texture_to_image(ctx->mlx,
-				tiles->idle[i].txt);
+		tiles->idle[i].img = mlx_texture_to_image(ctx->mlx, tiles->idle[i].txt);
 		if (!tiles->idle[i].img)
 			clean_exit(ctx, "Failed to load assets", errno);
 		mlx_resize_image(tiles->idle[i].img, 48, 48);
 	}
 	ft_printf("Loaded \n");
-}		
+}
 
 void	load_right(t_ctx *ctx, t_tiles *tiles)
 {
-	const char	*src[] = {"./textures/player/right/1.png", "./textures/player/right/2.png",
-		"./textures/player/right/3.png", "./textures/player/right/4.png", "./textures/player/right/5.png",NULL};
+	const char	*src[] = {"./textures/player/right/1.png",
+		"./textures/player/right/2.png", "./textures/player/right/3.png",
+		"./textures/player/right/4.png", "./textures/player/right/5.png",
+		NULL};
 	int			i;
 
 	tiles->right = malloc(sizeof(t_asset) * 5);
@@ -50,12 +65,14 @@ void	load_right(t_ctx *ctx, t_tiles *tiles)
 		mlx_resize_image(tiles->right[i].img, 48, 48);
 	}
 	ft_printf("Loaded \n");
-}		
+}
 
 void	load_left(t_ctx *ctx, t_tiles *tiles)
 {
-	const char	*src[] = {"./textures/player/left/1.png", "./textures/player/left/2.png",
-		"./textures/player/left/3.png", "./textures/player/left/4.png", "./textures/player/left/5.png",NULL};
+	const char	*src[] = {"./textures/player/left/1.png",
+		"./textures/player/left/2.png", "./textures/player/left/3.png",
+		"./textures/player/left/4.png", "./textures/player/left/5.png",
+		NULL};
 	int			i;
 
 	tiles->left = malloc(sizeof(t_asset) * 5);
@@ -69,8 +86,7 @@ void	load_left(t_ctx *ctx, t_tiles *tiles)
 		tiles->left[i].txt = mlx_load_png(src[i]);
 		if (!tiles->left[i].txt)
 			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->left[i].img = mlx_texture_to_image(ctx->mlx,
-				tiles->left[i].txt);
+		tiles->left[i].img = mlx_texture_to_image(ctx->mlx, tiles->left[i].txt);
 		if (!tiles->left[i].img)
 			clean_exit(ctx, "Failed to load assets", errno);
 		mlx_resize_image(tiles->left[i].img, 48, 48);
@@ -80,8 +96,10 @@ void	load_left(t_ctx *ctx, t_tiles *tiles)
 
 void	load_up(t_ctx *ctx, t_tiles *tiles)
 {
-	const char	*src[] = {"./textures/player/up/1.png", "./textures/player/up/2.png",
-		"./textures/player/up/3.png", "./textures/player/up/4.png", "./textures/player/up/5.png",NULL};
+	const char	*src[] = {"./textures/player/up/1.png",
+		"./textures/player/up/2.png", "./textures/player/up/3.png",
+		"./textures/player/up/4.png", "./textures/player/up/5.png",
+		NULL};
 	int			i;
 
 	tiles->up = malloc(sizeof(t_asset) * 5);
@@ -95,8 +113,7 @@ void	load_up(t_ctx *ctx, t_tiles *tiles)
 		tiles->up[i].txt = mlx_load_png(src[i]);
 		if (!tiles->up[i].txt)
 			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->up[i].img = mlx_texture_to_image(ctx->mlx,
-				tiles->up[i].txt);
+		tiles->up[i].img = mlx_texture_to_image(ctx->mlx, tiles->up[i].txt);
 		if (!tiles->up[i].img)
 			clean_exit(ctx, "Failed to load assets", errno);
 		mlx_resize_image(tiles->up[i].img, 48, 48);
@@ -106,8 +123,10 @@ void	load_up(t_ctx *ctx, t_tiles *tiles)
 
 void	load_down(t_ctx *ctx, t_tiles *tiles)
 {
-	const char	*src[] = {"./textures/player/down/1.png", "./textures/player/down/2.png",
-		"./textures/player/down/3.png", "./textures/player/down/4.png", "./textures/player/down/5.png",NULL};
+	const char	*src[] = {"./textures/player/down/1.png",
+		"./textures/player/down/2.png", "./textures/player/down/3.png",
+		"./textures/player/down/4.png", "./textures/player/down/5.png",
+		NULL};
 	int			i;
 
 	tiles->down = malloc(sizeof(t_asset) * 5);
@@ -121,8 +140,7 @@ void	load_down(t_ctx *ctx, t_tiles *tiles)
 		tiles->down[i].txt = mlx_load_png(src[i]);
 		if (!tiles->down[i].txt)
 			clean_exit(ctx, "Failed to load assets", errno);
-		tiles->down[i].img = mlx_texture_to_image(ctx->mlx,
-				tiles->down[i].txt);
+		tiles->down[i].img = mlx_texture_to_image(ctx->mlx, tiles->down[i].txt);
 		if (!tiles->down[i].img)
 			clean_exit(ctx, "Failed to load assets", errno);
 		mlx_resize_image(tiles->down[i].img, 48, 48);
