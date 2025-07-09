a3a4583bd8e61f2e07afb99c63aa51e534bdd069
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-09 19:56:12 +0300

    initializing player sprites

--- srcb/player_textures.c
+++ srcb/player_textures.c
@@ -7,4 +7,9 @@ void	load_player_textures(t_ctx *ctx, t_tiles *tiles)
 	load_down(ctx, tiles);
 	load_up(ctx, tiles);
 	load_left(ctx, tiles);
+	ctx->player->sprites[0] = ctx->map->tiles->up;
+	ctx->player->sprites[1] = ctx->map->tiles->down;
+	ctx->player->sprites[2] = ctx->map->tiles->left;
+	ctx->player->sprites[3] = ctx->map->tiles->right;
+	ctx->player->sprites[4] = ctx->map->tiles->idle;
 }
