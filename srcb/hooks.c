b2b55a4cc5d8e11ef1f9874a038fccf5a0ba16dd
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-10 17:54:08 +0300

    added func that updates the ui

--- srcb/hooks.c
+++ srcb/hooks.c
@@ -6,18 +6,21 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 21:23:53 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/09 21:23:57 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/10 17:45:57 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 static void	endgame(t_ctx *ctx);
+static void	update_steps(t_ctx *ctx);
+static void	update_ui(t_ctx *ctx);
 
 void	hooks(t_ctx *ctx)
 {
 	collect(ctx);
 	endgame(ctx);
+	update_steps(ctx);
 }
 
 void	key_hooks(mlx_key_data_t keydata, t_ctx *ctx)
@@ -40,4 +43,22 @@ static void	endgame(t_ctx *ctx)
 	}
 	if (check_collision(ctx, 'E', offsets, ctx->player->pos))
 		ctx->state->exited = true;
+}
+
+static void	update_ui(t_ctx *ctx)
+{
+	update_steps(ctx);
+}
+
+static void	update_steps(t_ctx *ctx)
+{
+	free(ctx->ui.steps.str);
+	mlx_delete_image(ctx->mlx, ctx->ui.steps.asset.img);
+	ctx->ui.steps.str = ft_strjoin("steps: ", ft_itoa(ctx->player->steps));
+	if (!ctx->ui.steps.str)
+		clean_exit(ctx, NULL, errno);
+	ctx->ui.steps.asset.img = mlx_put_string(ctx->mlx, ctx->ui.steps.str,
+			ctx->ui.steps.pos.x, ctx->ui.steps.pos.y);
+	if (!ctx->ui.steps.asset.img)
+		clean_exit(ctx, NULL, errno);
 }
