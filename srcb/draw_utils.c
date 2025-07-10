4c60b631d5515c95d4596dc0533a09accf056aa8
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-10 17:55:41 +0300

    added ui that counts the steps

--- srcb/draw_utils.c
+++ srcb/draw_utils.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 21:12:35 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/09 21:16:51 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/10 15:29:23y zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,27 @@ void	draw_enemy(t_ctx *ctx)
 		img_to_window(ctx, ctx->enemy->up[i].img, pos->x, pos->y);
 		img_to_window(ctx, ctx->enemy->down[i].img, pos->x, pos->y);
 	}
+}
+
+
+void    draw_ui(t_ctx *ctx)
+{
+    ctx->ui.steps.pos.x = 10;
+    ctx->ui.steps.pos.y = 4;
+	ctx->ui.steps.str = ft_strdup("steps: 0");
+	ctx->ui.steps.asset.txt = NULL;
+	if (!ctx->ui.steps.str)
+		clean_exit(ctx, NULL, errno);
+	ctx->ui.steps.asset.img = mlx_put_string(ctx->mlx, ctx->ui.steps.str, 10, 4);
+	if (!ctx->ui.steps.asset.img)
+		clean_exit(ctx, NULL, errno);
+	ctx->ui.collected.pos.x = 10 + 5;
+	// ctx->ui.collected.pos.y = 4;
+	//ctx->ui.collected.str = ft_strdup("Collected: 0");
+    // ctx->ui.collected.asset.txt = NULL;
+	// if (!ctx->ui.collected.str)
+	// 	clean_exit(ctx, NULL, errno);
+	//ctx->ui.collected.asset.img = mlx_put_string(ctx->mlx, ctx->ui.collected.str, 100, 4);
+	// if (!ctx->ui.collected.asset.img)
+	// 	clean_exit(ctx, NULL, errno);
 }
