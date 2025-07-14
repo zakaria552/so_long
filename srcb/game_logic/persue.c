e9d5465bd3d98a5be0c22f46eca0fbfe8bf4dcfa
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-14 23:46:28 +0300

    wip persue logic

--- srcb/game_logic/persue.c
+++ srcb/game_logic/persue.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 22:42:28 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/12 23:32:41 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/14 23:39:23 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,25 @@ void print_list(t_list *head)
 
 void    stay_alert(t_ctx *ctx)
 {
-    int x = ctx->enemy->pos.x / ctx->map->size;
-    int y = ctx->enemy->pos.y / ctx->map->size;
-    int px = ctx->player->pos->x / ctx->map->size;
-    int py = ctx->player->pos->y / ctx->map->size;
-    //char **grid = ctx->map->grid;
-    
-    print_list(ctx->enemy->path);
-    ft_printf("e(%d, %d) -- (%d, %d)", x, y, px, py);
-    if ((px == x - 1 && py == y ) || (px == x + 1 && py == y) || 
-        (px == x && py == y - 1) || (px == x && py == y + 1))
+    const t_vec2 *pos = ctx->player->pos;
+    const t_vec2 epos = ctx->enemy->pos;
+    const int x = ctx->enemy->pos.x / ctx->map->size;
+    const int y = ctx->enemy->pos.y / ctx->map->size;
+    const int px = ctx->player->pos->x / ctx->map->size;
+    const int py = ctx->player->pos->y / ctx->map->size;
+    const int d = ((pos->x - epos.x) * (pos->x - epos.x)) + ((pos->y - epos.y) * (pos->y - epos.y));
+
+    if (d < 150 * 150)
     {
+        ctx->enemy->dir = IDLE;
         ft_lstclear(&ctx->enemy->path, free);
-        ctx->enemy->path = dfs_target(ctx, target_vec(x, y), target_vec(px, py));
+        ctx->enemy->path = bfs_target(ctx, target_vec(x, y), target_vec(px, py));
+        if (ctx->enemy->path)
+        {
+            ctx->enemy->persuing = true;
+            ctx->enemy->speed = 2;
+        }
+        print_list2(ctx->enemy->path);
     }
     return;
 }
