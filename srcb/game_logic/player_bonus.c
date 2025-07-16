90805274842f86f4e3fe164d6d0666a861853513
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-16 18:35:58 +0300

    initializing vision img to null

--- srcb/game_logic/player_bonus.c
+++ srcb/game_logic/player_bonus.c
@@ -6,12 +6,13 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 15:48:01 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/15 23:09:48 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/16 18:35:44 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
+static void	init_player(t_ctx *ctx);
 static void	init_player_pos(t_ctx *ctx);
 
 void	initialize_player(t_ctx *ctx)
@@ -25,22 +26,11 @@ void	initialize_player(t_ctx *ctx)
 	ctx->player->vision = malloc(sizeof(t_vision));
 	if (!ctx->player->vision)
 		clean_exit(ctx, NULL, errno);
+	ctx->player->vision->img = NULL;
 	ctx->player->ani_info = malloc(sizeof(t_animation_info));
 	if (!ctx->player->ani_info)
 		clean_exit(ctx, NULL, errno);
-	ctx->player->orbs = 0;
-	ctx->player->speed = 6;
-	ctx->player->vision->r = 150;
-	ctx->player->idle = true;
-	ctx->player->ani_info->frame_index = 0;
-	ctx->player->ani_info->frame = 0;
-	ctx->player->ani_info->lt = 0;
-	ctx->player->dir = IDLE;
-	ctx->player->prev_dir = IDLE;
-	ctx->player->steps = 0;
-	init_player_pos(ctx);
-	ctx->player->grid_pos.x = ctx->player->pos->x;
-	ctx->player->grid_pos.y = ctx->player->pos->y;
+	init_player(ctx);
 }
 
 static void	init_player_pos(t_ctx *ctx)
@@ -64,4 +54,21 @@ static void	init_player_pos(t_ctx *ctx)
 			}
 		}
 	}
+}
+
+static void	init_player(t_ctx *ctx)
+{
+	ctx->player->orbs = 0;
+	ctx->player->speed = 6;
+	ctx->player->vision->r = 150;
+	ctx->player->idle = true;
+	ctx->player->ani_info->frame_index = 0;
+	ctx->player->ani_info->frame = 0;
+	ctx->player->ani_info->lt = 0;
+	ctx->player->dir = IDLE;
+	ctx->player->prev_dir = IDLE;
+	ctx->player->steps = 0;
+	init_player_pos(ctx);
+	ctx->player->grid_pos.x = ctx->player->pos->x;
+	ctx->player->grid_pos.y = ctx->player->pos->y;
 }
