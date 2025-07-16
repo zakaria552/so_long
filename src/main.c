a4b39731d8a5c72a83cc65b947a9fe708adb2d8f
zakaria.farah55@outlook.com <zakaria.farah55@outlook.com>
2025-07-16 15:44:49 +0300

    Removed custom msg on the exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 14:47:26 by zfarah            #+#    #+#             */
-/*   Updated: 2025/07/16 13:36:26 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/07/16 14:37:34 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **args)
 	ctx = ctx_init(args[1]);
 	initialize_player(ctx);
 	if (!valid_path_exists(ctx))
-		clean_exit(ctx, "Invalid map", errno);
+		clean_exit(ctx, NULL, errno);
 	ctx->mlx = mlx_init(ctx->map->width, ctx->map->height, args[0], true);
 	if (!ctx->mlx)
 		clean_exit(ctx, NULL, errno);
